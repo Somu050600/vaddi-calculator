fa05b5d3df9c790371fb4b73e7a7eb3f13e7e025
somu-aurva <somasekhara.eega@aurva.io>
2025-12-26 17:12:06 +0530

    add pwa offline suppourt

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,15 @@
-import Calculator from "@/components/calculator"
-import Header from "@/components/header"
-import { Toaster } from "@/components/ui/toaster"
+import ServiceWorkerRegister from "@/app/service-worker-register";
+import Calculator from "@/components/calculator";
+import Header from "@/components/header";
+import { Toaster } from "@/components/ui/toaster";
 
 export default function Home() {
   return (
     <main className="min-h-screen p-4 md:p-6 lg:p-8 max-w-4xl mx-auto">
+      <ServiceWorkerRegister />
       <Header />
       <Calculator />
       <Toaster />
     </main>
-  )
+  );
 }
